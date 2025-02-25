38ad2eb6cc653aa51f2f39d0d20af3cac01c5787
Serge Zaugg <serge.zaugg@gmail.com>
2025-02-25 08:42:03 +0100

    beta distrib now parametrised with mu and sigma, youpii

--- main.py
+++ main.py
@@ -9,44 +9,55 @@ import streamlit as st
 import plotly.express as px
 from sklearn.metrics import roc_auc_score
 
-def make_one_class_data(N, beta_a, beta_b, class_name):
-    vals = np.random.beta(a = beta_a, b = beta_b, size = N)
+
+
+# # reparamtrize 
+# # from https://www.johndcook.com/blog/2021/04/07/beta-given-mean-variance/
+# mu = np.random.uniform()
+# sigma2 = np.random.uniform(0, mu*(1-mu))
+
+
+# a = mu*(mu*(1-mu)/sigma2 - 1)
+# b = a*(1-mu)/mu
+
+def make_one_class_data(N, mu, sigma, class_name):
+    sigma2 = sigma**2
+    var_max = mu*(1-mu)
+    sdt_max = np.sqrt(var_max)
+    assert (sigma2 < var_max) , "sigma must be < " + str(sdt_max)
+    assert (sigma2 > 0 ) , "sigma must be > 0" 
+    a = mu*(mu*(1-mu)/sigma2 - 1)
+    b = a*(1-mu)/mu
+    vals = np.random.beta(a = a, b = b, size = N)
     df = pd.DataFrame({"proba_score":vals})
     df['class'] = class_name
     return(df)
 
+# test 
+mu_1 = 0.3
+sigma_1 = 0.45
+aaa = make_one_class_data(5000, mu_1, sigma_1, "aaa")
+[aaa['proba_score'].mean().round(2), mu_1]
+[aaa['proba_score'].std().round(2), sigma_1]
 
 
-# reparamtrize 
-# from https://www.johndcook.com/blog/2021/04/07/beta-given-mean-variance/
-mu = np.random.uniform()
-sigma2 = np.random.uniform(0, mu*(1-mu))
-a = mu*(mu*(1-mu)/sigma2 - 1)
-b = a*(1-mu)/mu
+
 
 
 N_1 = 5000
-beta_a_1 = a
-beta_b_1 = b
+mu_1 = 0.5
+sigma_1 = 0.02
 class_name_1 = "Class A"
 
-aaa = make_one_class_data(N_1, beta_a_1, beta_b_1, class_name_1)
-
-[aaa['proba_score'].mean(), mu]
-[aaa['proba_score'].var(), sigma2]
-
-
-
-
-
 N_2 = 500
-beta_a_2 = 3.0
-beta_b_2 = 1.0
+mu_2 = 0.8
+sigma_2 = 0.1
 class_name_2 = "Class B"
 
 df = pd.concat([
-    make_one_class_data(N_1, beta_a_1, beta_b_1, class_name_1),
-    make_one_class_data(N_2, beta_a_2, beta_b_2, class_name_2)
+    make_one_class_data(N_1, mu_1, sigma_1, class_name_1)
+    ,
+    make_one_class_data(N_2, mu_2, sigma_2, class_name_2)
     ])
 
 df['jitter'] = np.random.uniform(size=N_1+N_2)
