c2ca7ba5fb4a2515b065393c9d7fbc77c5e8bb57
Serge Zaugg <serge.zaugg@gmail.com>
2025-04-20 15:58:39 +0200

    some first success to make selected params conserved if user navigates

--- stmain.py
+++ stmain.py
@@ -11,11 +11,20 @@ st.set_page_config(layout="wide")
 
 # initial value of session state
 if 'color_a' not in ss:
-    ss.color_a = '#FF00AA'
+    ss["color_a"] = '#FF00AA'
 if 'color_b' not in ss:
     ss.color_b = '#6AFF00'
 if 'decision_thld' not in ss:
     ss.decision_thld = 0.5
+if 'upar' not in ss:
+    ss.upar = {
+        "N_1" : 1000,
+        "mu_1" : 0.20,
+        "sigma_1" : 0.20,
+
+        }
+
+
 
 # make navigation
 p0 = st.Page("st_page_00.py", title="Summary")
