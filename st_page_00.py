850ee18c79519a938ba53111c5d7a8963e195309
Serge Zaugg <serge.zaugg@gmail.com>
2025-05-04 09:23:27 +0200

    some final alignement and text improvement

--- st_page_00.py
+++ st_page_00.py
@@ -11,7 +11,6 @@ with col_aa:
 
     with st.container(border=True, key='conta_01'):
         st.title(":violet[Explain and understand machine learning performance metrics]") 
-        # st.subheader(":violet[Applied Machine Learning  ---  ML Tutorials  ---  Supervised Classification]") 
 
     with st.container(border=True, key='conta_02'):
         st.markdown(
--- st_page_01.py
+++ st_page_01.py
@@ -17,13 +17,11 @@ col_a1, col_a2, col_space011,= st.columns([0.20, 0.80, 0.05])
 
 # get user input
 with col_a1: 
-    with st.container(height=475, border=True, key='conta_01'):
+    with st.container(height=475, border=True):
         st.text("Simulate score distribution *")
         col_x1, col_x2, = st.columns([0.50, 0.50])
         with col_x1: 
-            st.text('Negative')
-            # st.text('Class')
-            # ss.upar['N_1'] = st.slider("N", min_value =  10, max_value=5000, step=10, value=ss.upar['N_1'], key = "Class_A_001", on_change=update_ss, args=["Class_A_001", "N_1"])
+            st.text('Negative ‡')
             ss.upar['N_1'] = st.number_input("N", min_value=1, max_value=10000, value=ss.upar['N_1'], step=10, key = "Class_A_001", on_change=update_ss, args=["Class_A_001", "N_1"])
             ss.upar['mu_1'] = st.slider("Mean", min_value = 0.03, max_value=0.97, value=ss.upar['mu_1'], label_visibility = "visible", key = "Class_A_002", on_change = update_ss, args=["Class_A_002", "mu_1"])
             # dynamically compute feasible upper std 
@@ -31,19 +29,15 @@ with col_a1:
             ss.upar['sigma_1'] = st.slider("Standard Deviation", min_value = 0.03, max_value=upper_lim, value=min(upper_lim, ss.upar['sigma_1']),  
                                            label_visibility = "visible", key = "Class_A_003", on_change = update_ss, args=["Class_A_003", "sigma_1"])
         with col_x2: 
-            st.text('Positive °')
-            # st.text('Class')
-            # ss.upar['N_2']     = st.slider("N", min_value =  10, max_value=5000, step=10, value=ss.upar['N_2'], key = "Class_B_001", on_change=update_ss, args=["Class_B_001", "N_2"])
+            st.text('Positive †')
             ss.upar['N_2'] = st.number_input("N", min_value=1, max_value=10000, value=ss.upar['N_2'], step=10, key = "Class_B_001", on_change=update_ss, args=["Class_B_001", "N_2"])
             ss.upar['mu_2']    = st.slider("Mean", min_value = 0.03, max_value=0.97, value=ss.upar['mu_2'], label_visibility = "visible", key = "Class_B_002", on_change=update_ss, args=["Class_B_002", "mu_2"])
             # dynamically compute feasible upper std 
             upper_lim = 0.90*np.sqrt(ss.upar['mu_2']*(1-ss.upar['mu_2'])) 
             ss.upar['sigma_2'] = st.slider("Standard Deviation", min_value = 0.03, max_value=upper_lim, value=min(upper_lim, ss.upar['sigma_2']),  
                                            label_visibility = "visible", key = "Class_B_003", on_change = update_ss, args=["Class_B_003", "sigma_2"])
-
-    # with st.container(height=None, border=True, key='conta_01b'):
-    #     ss["upar"]["dth"] = st.slider("Decision threshold", min_value= 0.0, max_value=1.0, value=ss["upar"]["dth"],  label_visibility = "visible", key="slide_07", on_change=update_ss, args=["slide_07", "dth"])
-
+        st.markdown("**\*** Beta distribution parametrized with mean and standard deviation is used for each class." )
+                    
     with st.container(height=None, border=True, key='conta_01c'):
         c1, c2 = st.columns([0.20, 0.20])
         with c1:
@@ -51,9 +45,12 @@ with col_a1:
         with c2:
             ss["upar"]["col_b"] = st.color_picker("Positive", ss["upar"]["col_b"])
             
-    st.text("""
-            * Beta distribution parametrized with mean and standard deviation used for each class. 
-            ° Positive class is the class to be detected.
+    st.markdown("""
+            **‡** **Negative** class is the combination of all that is not of primary interest, e.g. background noise, general landscape objects.   
+            
+            **†** **Positive** class is the class to be detected, e.g. call of a bird, person in an image.
+                
+            In epidemiology and diagnostic tests the **Negative** are the healthy subjects and **Positive** those that have the disease.  
             """)        
 
 # compute data, get perf metrics, and make plot 
@@ -63,18 +60,14 @@ df_metrics_free = get_metrics_thld_free(df = df)
 fig00 = make_fig(df = df, dot_colors = [ss["upar"]["col_a"], ss["upar"]["col_b"]])
 fig00.add_vline(x=ss["upar"]["dth"])
 
-
 # display plot and perf metrics 
 with col_a2:
-    with st.container(height=None, border=True):
+    with st.container(height=475, border=True):
         st.plotly_chart(fig00, use_container_width=True) 
         _, c2, _ = st.columns([0.01, 1.00, 0.01])
         with c2:
-            ss["upar"]["dth"] = st.slider("Decision threshold", min_value= 0.0, max_value=1.0, value=ss["upar"]["dth"],  label_visibility = "visible", key="slide_07", on_change=update_ss, args=["slide_07", "dth"])
-
+            ss["upar"]["dth"] = st.slider("Decision threshold", min_value= 0.0, max_value=1.0, value=ss["upar"]["dth"], 
+                                          key="slide_07", on_change=update_ss, args=["slide_07", "dth"])
     show_metrics(df_thld = df_metrics_thld, df_free = df_metrics_free)
 
   
-
-
-
